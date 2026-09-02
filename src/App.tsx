d064509bb6d06add2f10ceca66de5fc9db1e6382
AutoMech-Pro <quorintzylphia@gmail.com>
2026-09-02 11:28:54 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       {/* REMOVED basename here */}
-      <BrowserRouter>
+<BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
